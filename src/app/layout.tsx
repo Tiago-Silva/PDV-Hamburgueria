38cd2eaf5fd8c2ed42a221b49787e51ab37195f5
Tiago <ti.almeida58@gmail.com>
2024-02-22 11:20:37 -0300

    refactor: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
 'use client'
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import StyledJsxRegistry from "./registry";
-import { ThemeProvider } from "styled-components";
 
-import theme from "./global/theme";
 import { Providers } from "./Providers/providers";
-
-// const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata: Metadata = {
-//   title: "PDV - Hamburgueria",
-//   description: "PDV - para hamburgueria",
-// };
 
 export default function RootLayout({
   children,
