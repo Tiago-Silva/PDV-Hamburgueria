0818ca09ffb88f9027779d854a2eb6d6317e97f5
Tiago <ti.almeida58@gmail.com>
2024-04-04 15:56:07 -0300

    feat: create select custom

--- src/app/components/BoxToalizers/styles.ts
+++ src/app/components/BoxToalizers/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { FaUserFriends } from 'react-icons/fa';
+import React from "react";
 
 export const Container = styled.div`
   width: 40%;
@@ -25,8 +26,40 @@ export const WrapperTitles = styled.div`
 `;
 
 export const HeaderTitle = styled.h3`
-  font-size: 1.5vw;
+  font-size: 1.2vw;
   color: ${({ theme }) => theme.colors.text};
+`;
+
+export const WrapperSelect = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Select = styled.select`
+  appearance: none;
+  width: 100%;
+  background: transparent;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+
+  color: ${({ theme }) => theme.colors.text};
+  padding-left: 5px;
+  font-size: 1.2vw;
+  border: none;
+  margin-left: 10px;
+`;
+
+export const Option = styled.option<React.HTMLAttributes<HTMLOptionElement>>`
+  color: ${({ theme }) => theme.colors.text};
+  background: transparent;
+  display: flex;
+  white-space: pre;
+  min-height: 20px;
+  padding: 0px 2px 1px;
 `;
 
 export const Icon = styled(FaUserFriends)`
