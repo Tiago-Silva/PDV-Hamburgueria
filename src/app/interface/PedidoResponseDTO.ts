c255d749eecbc639aea98cc16ca4a15a30be3dd5
Tiago <ti.almeida58@gmail.com>
2024-04-10 14:42:32 -0300

    refactor: add user name

--- src/app/interface/PedidoResponseDTO.ts
+++ src/app/interface/PedidoResponseDTO.ts
@@ -4,6 +4,7 @@ import { ItemData } from "./ItemData";
 export interface PedidoResponseDTO {
   idpedido?: number;
   data?: string;
+  userName?: string;
   total: number;
   iduser: string;
   tipoPagamento: string;
