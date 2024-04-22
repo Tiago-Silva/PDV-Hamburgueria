849db2f83c6d773922437e8150ddf9da8e678cfd
Tiago <ti.almeida58@gmail.com>
2024-04-22 15:27:18 -0300

    feat: creation home page

---
+++ src/app/(pages)/Home/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import {Container} from "@/app/(pages)/Home/styles";
+
+const Home = () => {
+    return (
+        <Container>
+            <h1>Home</h1>
+        </Container>
+    );
+};
+
+export default Home;
